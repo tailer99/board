f0b1731ddaf9f31ef9be94a7e6a986716213a2a3
Danial Pyo <tailer9999@gmail.com>
2021-03-30 17:45:47 +0900

    error treatment add

---
+++ src/main/java/com/example/board/common/ExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.example.board.common;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.ModelAndView;
+
+import lombok.extern.slf4j.Slf4j;
+
+@ControllerAdvice
+@Slf4j
+public class ExceptionHandler {
+    
+    @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
+    public ModelAndView defaultExcetionHandler(HttpServletRequest request, Exception exception){
+        ModelAndView mv = new ModelAndView("/error/error_default");
+        mv.addObject("exception", exception);
+
+        log.error("exception", exception);
+
+        return mv;
+    }
+}
--- src/main/java/com/example/board/configuration/WebMvcConfiguration.java
+++ src/main/java/com/example/board/configuration/WebMvcConfiguration.java
@@ -1,8 +1,17 @@
 package com.example.board.configuration;
 
-import com.example.board.interceptor.LoggerInterceptor;
+import java.nio.charset.Charset;
 
+import javax.persistence.Cache;
+
+import com.example.board.interceptor.LoggerInterceptor;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,5 +22,20 @@ public class WebMvcConfiguration implements WebMvcConfigurer{
     public void addInterceptors(InterceptorRegistry registry){
         registry.addInterceptor(new LoggerInterceptor())
         .excludePathPatterns("/css/**", "/fonts/**", "/plugin/**", "/scripts/**");
+    }
+
+    @Bean
+    public CharacterEncodingFilter charactorEncodingFilter(){
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+
+        return characterEncodingFilter;
+    }
+
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter(){
+        return new StringHttpMessageConverter(Charset.forName("UTF-8"));
+
     }
 }
